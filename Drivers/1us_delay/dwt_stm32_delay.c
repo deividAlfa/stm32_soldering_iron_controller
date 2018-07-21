e9daab91c50299cddf186cd2d3af3cfebcfbe910
jomat <jomat@jmt.gr>
2018-07-21 20:59:40 +0000

    add Drivers/1us_delay

---
+++ Drivers/1us_delay/dwt_stm32_delay.c
@@ -0,0 +1,40 @@
+#include "dwt_stm32_delay.h"
+
+
+/**
+ * @brief  Initializes DWT_Clock_Cycle_Count for DWT_Delay_us function
+ * @return Error DWT counter
+ *         1: clock cycle counter not started
+ *         0: clock cycle counter works
+ */
+uint32_t DWT_Delay_Init(void) {
+  /* Disable TRC */
+  CoreDebug->DEMCR &= ~CoreDebug_DEMCR_TRCENA_Msk; // ~0x01000000;
+  /* Enable TRC */
+  CoreDebug->DEMCR |=  CoreDebug_DEMCR_TRCENA_Msk; // 0x01000000;
+
+  /* Disable clock cycle counter */
+  DWT->CTRL &= ~DWT_CTRL_CYCCNTENA_Msk; //~0x00000001;
+  /* Enable  clock cycle counter */
+  DWT->CTRL |=  DWT_CTRL_CYCCNTENA_Msk; //0x00000001;
+
+  /* Reset the clock cycle counter value */
+  DWT->CYCCNT = 0;
+
+     /* 3 NO OPERATION instructions */
+     __ASM volatile ("NOP");
+     __ASM volatile ("NOP");
+  __ASM volatile ("NOP");
+
+  /* Check if clock cycle counter has started */
+     if(DWT->CYCCNT)
+     {
+       return 0; /*clock cycle counter started*/
+     }
+     else
+  {
+    return 1; /*clock cycle counter not started*/
+  }
+}
+
+/* Use DWT_Delay_Init (); and DWT_Delay_us (microseconds) in the main */
---
+++ Drivers/1us_delay/dwt_stm32_delay.h
@@ -0,0 +1,41 @@
+#ifndef DWT_STM32_DELAY_H
+#define DWT_STM32_DELAY_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#include "stm32f1xx_hal.h"
+
+/**
+ * @brief  Initializes DWT_Cycle_Count for DWT_Delay_us function
+ * @return Error DWT counter
+ *         1: DWT counter Error
+ *         0: DWT counter works
+ */
+uint32_t DWT_Delay_Init(void);
+
+
+
+/**
+ * @brief  This function provides a delay (in microseconds)
+ * @param  microseconds: delay in microseconds
+ */
+__STATIC_INLINE void DWT_Delay_us(volatile uint32_t microseconds)
+{
+  uint32_t clk_cycle_start = DWT->CYCCNT;
+
+  /* Go to number of cycles for system */
+  microseconds *= (HAL_RCC_GetHCLKFreq() / 1000000);
+
+  /* Delay till end */
+  while ((DWT->CYCCNT - clk_cycle_start) < microseconds);
+}
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
