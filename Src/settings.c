731007697ed80876da0177d71ac228892e456835
mickaustomo@gmail.com <Jolantut3>
2020-02-29 14:44:07 +0200

    prj settings flash 64kb-128kb

--- Src/settings.c
+++ Src/settings.c
@@ -30,13 +30,11 @@
 //			},
 //};
 
-systemSettings_t systemSettings = { .ironTips[0 ... MAX_IRON_TIPS - 1] = {
-		"DFLT", DFTL_PARAMS }, };
+systemSettings_t systemSettings = { .ironTips[0 ... MAX_IRON_TIPS - 1] = { "DFLT", DFTL_PARAMS }, };
 
-#define FLASH_ADDR (0x8000000|(0x10000-FLASH_PAGE_SIZE))/*Flash start OR'ed with the maximum amount of flash - 256 bytes*/
+#define FLASH_ADDR ( (FLASH_BANK1_END+1)-FLASH_PAGE_SIZE)/*Flash start OR'ed with the maximum amount of flash - 256 bytes*/
 
-
-_Static_assert(  (sizeof(systemSettings_t)<=FLASH_PAGE_SIZE) , "systemSettings_t dont fit in FLASH_PAGE_SIZE");
+_Static_assert( (sizeof(systemSettings_t)<=FLASH_PAGE_SIZE) , "systemSettings_t dont fit in FLASH_PAGE_SIZE");
 
 void saveSettings() {
 	HAL_FLASH_Unlock(); //unlock flash writing
@@ -51,23 +49,20 @@ void saveSettings() {
 	HAL_FLASHEx_Erase(&erase, &error);
 	uint16_t *data = (uint16_t*) &systemSettings;
 	for (uint16_t i = 0; i < (sizeof(systemSettings) / 2); i++) {
-		HAL_FLASH_Program(FLASH_TYPEPROGRAM_HALFWORD, FLASH_ADDR + (i * 2),
-				data[i]);
+		HAL_FLASH_Program(FLASH_TYPEPROGRAM_HALFWORD, FLASH_ADDR + (i * 2), data[i]);
 	}
 	HAL_FLASH_Lock();
 }
 
 void restoreSettings() {
-	memcpy(&systemSettings, (void*) FLASH_ADDR, sizeof(systemSettings));
-//	uint16_t *data = (uint16_t*) &systemSettings;
-//	for (uint16_t i = 0; i < (sizeof(systemSettings) / 2); i++) {
-//		data[i] = *(uint16_t *) (FLASH_ADDR + (i * 2));
-//	}
-	uint16_t calc_crc16 = crc16_compute((uint8_t*) &systemSettings,
-			offsetof(systemSettings_t, crc16), NULL);
+	//memcpy(&systemSettings, (void*) FLASH_ADDR, sizeof(systemSettings));
+	uint16_t *data = (uint16_t*) &systemSettings;
+	for (uint16_t i = 0; i < (sizeof(systemSettings) / 2); i++) {
+		data[i] = *(uint16_t *) (FLASH_ADDR + (i * 2));
+	}
+	uint16_t calc_crc16 = crc16_compute((uint8_t*) &systemSettings, offsetof(systemSettings_t, crc16), NULL);
 	//resetSettings();
-	if (systemSettings.version != SETTINGSVERSION
-			|| calc_crc16 != systemSettings.crc16) {
+	if (systemSettings.version != SETTINGSVERSION || calc_crc16 != systemSettings.crc16) {
 		resetSettings();
 		saveSettings();
 	}
@@ -104,27 +99,18 @@ void resetSettings() {
 //		systemSettings.ironTips[x] = a;
 //	}
 	int idx = 0;
-	systemSettings.ironTips[idx++] = (tipData ) { "B", 1300, 2100, 2600, {
-					0.0015, 0.002, 0, DF_LIMS } };
-	systemSettings.ironTips[idx++] = (tipData ) { "BC2", 1200, 2000, 3100, {
-					0.003, 0.0025, 0, DF_LIMS } };
-	systemSettings.ironTips[idx++] = (tipData ) { "BL", 1100, 1800, 2800, {
-					0.0015, 0.002, 0, DF_LIMS } };
+	systemSettings.ironTips[idx++] = (tipData ) { "B", 1300, 2100, 2600, { 0.0015, 0.002, 0, DF_LIMS } };
+	systemSettings.ironTips[idx++] = (tipData ) { "BC2", 1200, 2000, 3100, { 0.003, 0.0025, 0, DF_LIMS } };
+	systemSettings.ironTips[idx++] = (tipData ) { "BL", 1100, 1800, 2800, { 0.0015, 0.002, 0, DF_LIMS } };
 	//systemSettings.ironTips[idx++] = (tipData){"BCF1"	, 1100 , 1800	,2800 , { 0.001 , 0.0005 	,   	0, DF_LIMS }};
 	//systemSettings.ironTips[idx++] = (tipData){"C1"		, 1450 , 2300	,2800 , { 0.0015 , 0.003 	,   	0, DF_LIMS }};
 	//systemSettings.ironTips[idx++] = (tipData){"C2"		, DFTL_PARAMS };
-	systemSettings.ironTips[idx++] = (tipData ) { "C4", 1200, 2600, 2900, {
-					0.002, 0.003, 0, DF_LIMS } };
-	systemSettings.ironTips[idx++] = (tipData ) { "D16", 1200, 2000, 2600, {
-					0.003, 0.003 * 2, 0, DF_LIMS } };
-	systemSettings.ironTips[idx++] = (tipData ) { "D24", 1300, 2300, 3000, {
-					0.003, 0.003 * 2, 0, DF_LIMS } };
-	systemSettings.ironTips[idx++] = (tipData ) { "ILS", 1400, 2150, 2800, {
-					0.001, 0.003, 0, DF_LIMS } };
-	systemSettings.ironTips[idx++] = (tipData ) { "406", 1000, 2000, 3100, {
-					0.001, 0.001, 0, DF_LIMS } };
-	systemSettings.ironTips[idx++] = (tipData ) { "403", 1000, 2000, 3100, {
-					0.001, 0.001, 0, DF_LIMS } };
+	systemSettings.ironTips[idx++] = (tipData ) { "C4", 1200, 2600, 2900, { 0.002, 0.003, 0, DF_LIMS } };
+	systemSettings.ironTips[idx++] = (tipData ) { "D16", 1200, 2000, 2600, { 0.003, 0.003 * 2, 0, DF_LIMS } };
+	systemSettings.ironTips[idx++] = (tipData ) { "D24", 1300, 2300, 3000, { 0.003, 0.003 * 2, 0, DF_LIMS } };
+	systemSettings.ironTips[idx++] = (tipData ) { "ILS", 1400, 2150, 2800, { 0.001, 0.003, 0, DF_LIMS } };
+	systemSettings.ironTips[idx++] = (tipData ) { "406", 1000, 2000, 3100, { 0.001, 0.001, 0, DF_LIMS } };
+	systemSettings.ironTips[idx++] = (tipData ) { "403", 1000, 2000, 3100, { 0.001, 0.001, 0, DF_LIMS } };
 
 	systemSettings.currentNumberOfTips = idx;
 
