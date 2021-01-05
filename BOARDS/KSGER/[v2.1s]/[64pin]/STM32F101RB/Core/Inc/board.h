a108de3c68be99b4aa28b2b2ce387bcd5d75eab3
DeividAlfa <48562135+deividAlfa@users.noreply.github.com>
2021-01-05 21:36:38 +0100

    Deleted deprecated define Soft_SPI

--- BOARDS/KSGER/[v2.1s]/[64pin]/STM32F101RB/Core/Inc/board.h
+++ BOARDS/KSGER/[v2.1s]/[64pin]/STM32F101RB/Core/Inc/board.h
@@ -15,9 +15,6 @@
 //#define USE_FILTER							                // Comment to totally disable filtering (Only make average of the last buffer)
 //#define FILTER_N				3			                    // For filter coefficient (Higher, more filtering, also more delay in the filter output, can make the system oscillate)
 
-                                                                // If your screen doesn't work, and want to discard a SPI problem
-//#define Soft_SPI							                    // Uncomment to disable Hardware SPI with DMA and use software SPI
-
 /********************************
  * 			PWM Settings        *
  ********************************/
