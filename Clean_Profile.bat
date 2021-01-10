3c0ec9a1f0edf5f5b403a6e2db05895785fba208
DeividAlfa <48562135+deividAlfa@users.noreply.github.com>
2021-01-10 15:57:24 +0100

    Added cleanup script

---
+++ Clean_Profile.bat
@@ -0,0 +1,14 @@
+@echo off
+del .\Core\Inc\stm*.* /Q >nul
+del .\Core\Inc\board.h /Q >nul
+del .\Core\Src\stm*.* /Q >nul
+del .\Core\Src\system_stm*.* /Q >nul
+del .\Core\Startup\*.s /Q >nul
+del .\.cproject /Q >nul
+del .\.project /Q >nul
+del .\STM32SolderingStation.ioc /Q >nul
+rmdir .\Debug /S /Q >nul
+rmdir .\Release /S /Q >nul
+echo Done cleaning profile files!
+echo Press any key to close the window...
+pause >nul
