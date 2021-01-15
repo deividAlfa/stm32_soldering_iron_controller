a82794c3ba4824dfa510a2ef6401ad813f08c5a2
DeividAlfa <48562135+deividAlfa@users.noreply.github.com>
2021-01-15 10:35:27 +0100

    Long pressing the main screen iron tips widget enters the PID screen directly.
    Then, exiting the PID menu returns to the original screen where it was
    called from.

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -28,6 +28,7 @@ extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
+#include "stm32f0xx_hal.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -36,6 +37,7 @@ extern "C" {
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 /* USER CODE END Includes */
@@ -75,7 +77,37 @@ void Program_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-
+#define OLED_CS_Pin GPIO_PIN_13
+#define OLED_CS_GPIO_Port GPIOC
+#define VREF_Pin GPIO_PIN_1
+#define VREF_GPIO_Port GPIOA
+#define NTC_Pin GPIO_PIN_2
+#define NTC_GPIO_Port GPIOA
+#define V_INPUT_Pin GPIO_PIN_3
+#define V_INPUT_GPIO_Port GPIOA
+#define IRON_TEMP_Pin GPIO_PIN_5
+#define IRON_TEMP_GPIO_Port GPIOA
+#define OLED_DC_Pin GPIO_PIN_11
+#define OLED_DC_GPIO_Port GPIOB
+#define OLED_RST_Pin GPIO_PIN_12
+#define OLED_RST_GPIO_Port GPIOB
+#define SCK_Pin GPIO_PIN_13
+#define SCK_GPIO_Port GPIOB
+#define SDO_Pin GPIO_PIN_15
+#define SDO_GPIO_Port GPIOB
+#define ROT_ENC_R_Pin GPIO_PIN_8
+#define ROT_ENC_R_GPIO_Port GPIOA
+#define ROT_ENC_L_Pin GPIO_PIN_9
+#define ROT_ENC_L_GPIO_Port GPIOA
+#define WAKE_Pin GPIO_PIN_10
+#define WAKE_GPIO_Port GPIOA
+#define WAKE_EXTI_IRQn EXTI4_15_IRQn
+#define ROT_ENC_BUTTON_Pin GPIO_PIN_11
+#define ROT_ENC_BUTTON_GPIO_Port GPIOA
+#define BUZZER_Pin GPIO_PIN_12
+#define BUZZER_GPIO_Port GPIOA
+#define PWM_Pin GPIO_PIN_7
+#define PWM_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 #define WAKE_input			HAL_GPIO_ReadPin(WAKE_GPIO_Port, WAKE_Pin)
 #define BUTTON_input		HAL_GPIO_ReadPin(ROT_ENC_BUTTON_GPIO_Port, ROT_ENC_BUTTON_Pin)
