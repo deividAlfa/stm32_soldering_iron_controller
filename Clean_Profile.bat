004ecba0cb145844e254bb4d37206f58c785d351
deividAlfa <48562135+deividAlfa@users.noreply.github.com>
2021-02-06 00:05:02 +0100

    Updated all builds

--- Clean_Profile.bat
+++ Clean_Profile.bat
@@ -7,6 +7,8 @@ del .\Core\Startup\*.s /Q >nul
 del .\.cproject /Q >nul
 del .\.project /Q >nul
 del .\STM32SolderingStation.ioc /Q >nul
+rmdir .\Drivers\STM32F0xx_HAL_Driver /S /Q >nul
+rmdir .\Drivers\STM32F1xx_HAL_Driver /S /Q >nul
 rmdir .\Debug /S /Q >nul
 rmdir .\Release /S /Q >nul
 echo Done cleaning profile files!
