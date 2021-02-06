150e6245184b5dbcd451bc48eb23b657315d8976
deividAlfa <48562135+deividAlfa@users.noreply.github.com>
2021-02-06 20:31:41 +0100

    Set all builds for 64KB flash by default

--- BOARDS/KSGER/[v3.1]/[64pin]/STM32F102R8/Core/Inc/board.h
+++ BOARDS/KSGER/[v3.1]/[64pin]/STM32F102R8/Core/Inc/board.h
@@ -72,7 +72,7 @@
  ********************************/
 #define HIWDG				hiwdg							// iwdg used
 #define HCRC				hcrc							// crc used
-#define FLASH_SZ			128								// Flash Size (KB)
+#define FLASH_SZ			64								// Flash Size (KB)
 //#define NOSAVESETTINGS									// Don't use flash to save or load settings. Always use defaults (for debugging purposes)
 
 
