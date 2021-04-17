2c13bcd0585232325abf744dc29272ef8ea3ae2a
deividAlfa <48562135+deividAlfa@users.noreply.github.com>
2021-04-17 18:30:10 +0200

    This PID values seem to do right

--- Core/Src/settings.c
+++ Core/Src/settings.c
@@ -184,11 +184,11 @@ void resetCurrentProfile(void){
 			systemSettings.Profile.tip[x].calADC_At_250	= T12_Cal250;
 			systemSettings.Profile.tip[x].calADC_At_350	= T12_Cal350;			// These values are way lower, but better to be safe than sorry
 			systemSettings.Profile.tip[x].calADC_At_450	= T12_Cal450;			// User needs to calibrate its station
-			systemSettings.Profile.tip[x].PID.Kp 		    = 5200;           // val = /1.000.000
+			systemSettings.Profile.tip[x].PID.Kp 		    = 6500;           // val = /1.000.000
 			systemSettings.Profile.tip[x].PID.Ki 		    = 2500;           // val = /1.000.000
-			systemSettings.Profile.tip[x].PID.Kd 		    = 1400;           // val = /1.000.000
-			systemSettings.Profile.tip[x].PID.maxI	    = 50;             // val = /100
-			systemSettings.Profile.tip[x].PID.minI 	    = -30;            // val = /100
+			systemSettings.Profile.tip[x].PID.Kd 		    = 3000;           // val = /1.000.000
+			systemSettings.Profile.tip[x].PID.maxI	    = 30;             // val = /100
+			systemSettings.Profile.tip[x].PID.minI 	    = 0;              // val = /100
 		}
 		strcpy(systemSettings.Profile.tip[0].name, "T12 ");
 		systemSettings.Profile.currentNumberOfTips  = 1;
@@ -208,11 +208,11 @@ void resetCurrentProfile(void){
 			systemSettings.Profile.tip[x].calADC_At_250 = C245_Cal250;
 			systemSettings.Profile.tip[x].calADC_At_350 = C245_Cal350;
 			systemSettings.Profile.tip[x].calADC_At_450 = C245_Cal450;
-			systemSettings.Profile.tip[x].PID.Kp 		    = 2800;
-			systemSettings.Profile.tip[x].PID.Ki 		    = 1800;
-			systemSettings.Profile.tip[x].PID.Kd 		    = 700;
-			systemSettings.Profile.tip[x].PID.maxI 	    = 50;
-			systemSettings.Profile.tip[x].PID.minI 	    = -30;
+      systemSettings.Profile.tip[x].PID.Kp        = 6500;           // val = /1.000.000
+      systemSettings.Profile.tip[x].PID.Ki        = 2500;           // val = /1.000.000
+      systemSettings.Profile.tip[x].PID.Kd        = 3000;           // val = /1.000.000
+      systemSettings.Profile.tip[x].PID.maxI      = 30;             // val = /100
+      systemSettings.Profile.tip[x].PID.minI      = 0;              // val = /100
 		}
 		strcpy(systemSettings.Profile.tip[0].name, "C245");
 		systemSettings.Profile.currentNumberOfTips	= 1;
@@ -231,11 +231,11 @@ void resetCurrentProfile(void){
 			systemSettings.Profile.tip[x].calADC_At_250 = C210_Cal250;
 			systemSettings.Profile.tip[x].calADC_At_350 = C210_Cal350;
 			systemSettings.Profile.tip[x].calADC_At_450 = C210_Cal450;
-			systemSettings.Profile.tip[x].PID.Kp 		    = 2800;
-			systemSettings.Profile.tip[x].PID.Ki 		    = 1800;
-			systemSettings.Profile.tip[x].PID.Kd 		    = 700;
-			systemSettings.Profile.tip[x].PID.maxI 	    = 50;
-			systemSettings.Profile.tip[x].PID.minI 	    = -30;
+      systemSettings.Profile.tip[x].PID.Kp        = 6500;           // val = /1.000.000
+      systemSettings.Profile.tip[x].PID.Ki        = 2500;           // val = /1.000.000
+      systemSettings.Profile.tip[x].PID.Kd        = 3000;           // val = /1.000.000
+      systemSettings.Profile.tip[x].PID.maxI      = 30;             // val = /100
+      systemSettings.Profile.tip[x].PID.minI      = 0;              // val = /100
 		}
 		strcpy(systemSettings.Profile.tip[0].name, "C210");
 		systemSettings.Profile.currentNumberOfTips  = 1;
