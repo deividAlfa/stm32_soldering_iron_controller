8b017a92552992322cf77dbba94bee51a05a7533
deividAlfa <48562135+deividAlfa@users.noreply.github.com>
2021-05-09 13:27:37 +0200

    Add "HOT!" characters to main screen label font

--- Drivers/graphics/u8g2/tools/font/bdfconv/make_labels.bat
+++ Drivers/graphics/u8g2/tools/font/bdfconv/make_labels.bat
@@ -1,1 +1,1 @@
-bdfconv_2_22.exe -v -f 1 -m "45,46,48-57,67,70,86,176" ../bdf/wizzard12_.bdf -o .\c\labels.c  -n u8g2_font_labels
+bdfconv_2_22.exe -v -f 1 -m "33,45,46,48-57,67,70,72,79,84,86,176" ../bdf/wizzard12_.bdf -o .\c\labels.c  -n u8g2_font_labels
