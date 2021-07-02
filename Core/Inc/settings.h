273d681aa8fecad65806f8f3a54c183248158ef1
deividAlfa <48562135+deividAlfa@users.noreply.github.com>
2021-07-02 08:20:27 +0200

    Update version

--- Core/Inc/settings.h
+++ Core/Inc/settings.h
@@ -31,7 +31,7 @@
 #define C245_Cal450       1100
 
 //#define SWSTRING        "SW: v1.10"                               // For releases
-#define SWSTRING          "SW: 2021-06-27"                          // For git
+#define SWSTRING          "SW: 2021-07-02"                          // For git
 #define SETTINGS_VERSION  3                                         // Change this if you change the struct below to prevent people getting out of sync
 #define StoreSize         2                                         // In KB
 #define FLASH_ADDR     (0x8000000 + ((FLASH_SZ-StoreSize)*1024))    // Last 2KB flash (Minimum erase size, page size=2KB)
