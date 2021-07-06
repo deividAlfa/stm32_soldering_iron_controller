8af0870061780262e1b5b6aeba22fa1ec7eeda33
deividAlfa <48562135+deividAlfa@users.noreply.github.com>
2021-07-06 02:27:22 +0200

    Enhance return on long click behaviour

--- Drivers/graphics/gui/settings_screen.c
+++ Drivers/graphics/gui/settings_screen.c
@@ -613,6 +613,25 @@ static void * getShakeWake() {
 // Settings screen functions
 //-------------------------------------------------------------------------------------------------------------------------------
 
+
+static int longClickReturn(widget_t *w){
+  selectable_widget_t *sel=NULL;
+  if(w->type!=widget_combo){
+    extractSelectablePartFromWidget(w);
+  }
+  else{
+    comboBox_item_t *combo =  ((comboBox_widget_t*)w->content)->currentItem;
+    if((combo->type == combo_Editable) || (combo->type == combo_MultiOption)){
+      sel = &combo->widget->selectable;
+    }
+  }
+  if(!sel || (sel && (sel->state!=widget_edit))){
+      return screen_main;
+  }
+  return -1;
+}
+
+
 static void settings_screen_init(screen_t *scr) {
   default_init(scr);
   scr->current_widget = &comboWidget_Settings;
@@ -630,14 +649,19 @@ int settings_screenProcessInput(screen_t * scr, RE_Rotation_t input, RE_State_t 
   if(input!=Rotate_Nothing){
     settingsTimer=HAL_GetTick();
   }
+  if(input==LongClick){
+    int x = longClickReturn(scr->current_widget);
+    if (x!=-1){
+      return x;
+    }
+  }
 
-  if((input==LongClick) || (HAL_GetTick()-settingsTimer)>15000){
+  if((HAL_GetTick()-settingsTimer)>15000){
     return screen_main;
   }
 
   return default_screenProcessInput(scr, input, state);
 }
-
 
 
 //-------------------------------------------------------------------------------------------------------------------------------
@@ -768,12 +792,19 @@ int IRONTIPS_Settings_ProcessInput(screen_t * scr, RE_Rotation_t input, RE_State
   static uint32_t last_update=0;
   uint32_t currentTime=HAL_GetTick();
   bool update=0;
-  if((input==LongClick) || (HAL_GetTick()-settingsTimer)>30000){
+  if(input==LongClick){
+    int x = longClickReturn(scr->current_widget);
+    if (x!=-1){
+      return x;
+    }
+  }
+  else if((currentTime-settingsTimer)>30000){
     return screen_main;
   }
+  
   if(input!=Rotate_Nothing){
     settingsTimer=currentTime;
-    if(editable_IRONTIPS_Settings_TipLabel.selectable.state==widget_edit){                                      // If tip name is being edited and there's encoder activity
+    if(editable_IRONTIPS_Settings_TipLabel.selectable.state==widget_edit){
       update=1;
     }
   }
