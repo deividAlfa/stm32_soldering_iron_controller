32cfa2b6ea46c27b0a8dd23e1b43ca28bf8f56d9
deividAlfa <48562135+deividAlfa@users.noreply.github.com>
2021-07-28 01:55:38 +0200

    Increase T12 pid values, filter factor 4.

--- Core/Src/settings.c
+++ Core/Src/settings.c
@@ -285,10 +285,10 @@ void resetCurrentProfile(void){
       systemSettings.Profile.tip[x].calADC_At_250   = T12_Cal250;
       systemSettings.Profile.tip[x].calADC_At_350   = T12_Cal350;     // These values are way lower, but better to be safe than sorry
       systemSettings.Profile.tip[x].calADC_At_450   = T12_Cal450;     // User needs to calibrate its station
-      systemSettings.Profile.tip[x].PID.Kp          = 7500;           // val = /1.000.000
-      systemSettings.Profile.tip[x].PID.Ki          = 4000;           // val = /1.000.000
+      systemSettings.Profile.tip[x].PID.Kp          = 10000;           // val = /1.000.000
+      systemSettings.Profile.tip[x].PID.Ki          = 6000;           // val = /1.000.000
       systemSettings.Profile.tip[x].PID.Kd          = 1000;           // val = /1.000.000
-      systemSettings.Profile.tip[x].PID.maxI        = 40;             // val = /100
+      systemSettings.Profile.tip[x].PID.maxI        = 60;             // val = /100
       systemSettings.Profile.tip[x].PID.minI        = 0;              // val = /100
       systemSettings.Profile.tip[x].PID.tau         = 10;             // val = /100
       strcpy(systemSettings.Profile.tip[x].name, _BLANK_TIP);         // Empty name
