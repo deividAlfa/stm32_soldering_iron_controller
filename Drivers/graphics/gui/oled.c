8dc32f0d97ddabd333ea08d7a4a9afb2f80ebea7
deividAlfa <48562135+deividAlfa@users.noreply.github.com>
2021-07-30 23:14:15 +0200

    Implement dynamic screen loading.
    Add automatic comboBox status saving and restore.
    Set screen defaults when adding it doing it later.
    Ignore processInput result -2 (Not processed)

--- Drivers/graphics/gui/oled.c
+++ Drivers/graphics/gui/oled.c
@@ -2,15 +2,18 @@
  * oled.c
  *
  *  Created on: Jan 12, 2021
- *      Author: David    Original work by Jose (PTDreamer), 2017
+ *      Author: David    Original work by Jose Barros (PTDreamer), 2017
  */
 
 
 #include <stdlib.h>
 #include "oled.h"
+#include <malloc.h>
+
+struct mallinfo mi;
 
 static screen_t *screens = NULL;
-static screen_t *current_screen;
+screen_t *current_screen;
 static RE_State_t* RE_State;
 
 RE_Rotation_t (*RE_GetData)(RE_State_t*);
@@ -18,13 +21,14 @@ RE_Rotation_t RE_Rotation;
 
 
 void oled_addScreen(screen_t *screen, uint8_t index) {
+  screen->processInput = &default_screenProcessInput;
+  screen->init = &default_init;
+  screen->draw = &default_screenDraw;
+  screen->update = &default_screenUpdate;
+  screen->onEnter = NULL;
+  screen->onExit = NULL;
   screen->index = index;
   screen->next_screen = NULL;
-  screen->init = NULL;
-  screen->draw = NULL;
-  screen->onExit = NULL;
-  screen->onEnter = NULL;
-  screen->processInput = NULL;
   screen->widgets = NULL;
   screen->current_widget = NULL;
   if(screens == NULL) {
@@ -59,6 +63,11 @@ void oled_init(RE_Rotation_t (*GetData)(RE_State_t*), RE_State_t *State) {
   screen_t *scr = screens;
   while(scr) {
     if(scr->index == 0) {
+      if(scr->create){                                // Create entering screen
+        mi = mallinfo();
+        scr->create(scr);
+        mi = mallinfo();
+      }
       scr->init(scr);
       current_screen = scr;
       return;
@@ -67,25 +76,116 @@ void oled_init(RE_Rotation_t (*GetData)(RE_State_t*), RE_State_t *State) {
 }
 static RE_State_t* RE_State;
 
+// Free all screen resources after exiting it.
+void oled_destroy_screen(screen_t *scr){
+  widget_t *w = scr->widgets;
+  widget_t *next;
+  if(!w){ return; }
+  do{
+    next=w->next_widget;
+    if(w->content){
+      if(w->type==widget_combo){
+        comboBox_item_t *Item = ((comboBox_widget_t*)w->content)->first;
+        comboBox_item_t *Next;
+        if(Item){
+          do{
+            Next=Item->next_item;
+            if( (Item->type==combo_Editable) || (Item->type==combo_MultiOption)){
+              free(Item->widget);
+            }
+            free(Item);
+            Item=Next;
+          }while(Next);
+        }
+      }
+      free(w->content);
+    }
+    free(w);
+    w = next;
+  }while(next);
+  scr->current_widget=NULL;
+  scr->widgets=NULL;
+}
 
+void oled_backup_comboStatus(screen_t *scr){
+  if((scr->widgets)&&(scr->widgets->type==widget_combo)){
+    comboBox_widget_t *combo = (comboBox_widget_t*)scr->widgets->content;
+    comboBox_item_t *item=combo->first;
+    uint8_t t=0;
+
+    while(item){
+      if(item==combo->currentItem){
+        break;
+      }
+      item=item->next_item;
+      t++;
+    }
+    if(!item){
+      scr->backup_combo_index=0;
+      scr->backup_combo_scroll=0;
+    }
+    else{
+      scr->backup_combo_scroll=combo->currentScroll;
+      scr->backup_combo_index=t;
+    }
+  }
+}
+void oled_restore_comboStatus(screen_t *scr){
+  if((scr->widgets)&&(scr->widgets->type==widget_combo)){
+    comboBox_widget_t *combo = (comboBox_widget_t*)scr->widgets->content;
+    comboBox_item_t *item=combo->first;
+    uint8_t t = scr->backup_combo_index;
+    while(t--){
+      if(!item->next_item){
+        return;
+      }
+      item=item->next_item;
+    }
+    combo->currentScroll=scr->backup_combo_scroll;
+    combo->currentItem=item;
+  }
+}
 void oled_processInput(void) {
   RE_Rotation = (*RE_GetData)(RE_State);
   int ret = current_screen->processInput(current_screen, RE_Rotation, RE_State);
-  if(ret != -1) {
+  if(ret > 0) {   // -1 do nothing, -2 nothing processed
     screen_t *scr = screens;
+
+
     while(scr) {
       if(scr->index == ret) {
         FillBuffer(BLACK, fill_dma);
-        scr->refresh=screen_Erased;       // Changed screen, force full display erase using dma
-        if(current_screen->onExit)
+        scr->refresh=screen_Erased;
+
+        if(current_screen->onExit){
           current_screen->onExit(scr);
-        if(scr->onEnter)
-          scr->onEnter(current_screen);
+        }
+
+        if(current_screen->create){                     // If "create" exists,it means it's a dynamic screen
+          oled_backup_comboStatus(current_screen);      // Save combo position
+          mi = mallinfo();
+          oled_destroy_screen(current_screen);          // Destroy exiting screen
+          mi = mallinfo();
+        }
+
+        if(scr->create){                                // Create entering screen
+          scr->create(scr);
+          mi = mallinfo();
+          oled_restore_comboStatus(scr);                // Restore combo position
+        }
+
         scr->init(scr);
-        if(scr->update)
-          scr->update(scr);
+
+        mi = mallinfo();
         RE_Rotation=Rotate_Nothing;                     // Force first pass without activity to update screen
         scr->processInput(scr, RE_Rotation, RE_State);
+        if(scr->onEnter){
+          scr->onEnter(current_screen);
+        }
+        if(scr->update){
+          scr->update(scr);
+        }
+
 
         current_screen = scr;
         return;
--- Drivers/graphics/gui/oled.h
+++ Drivers/graphics/gui/oled.h
@@ -2,14 +2,14 @@
  * oled.h
  *
  *  Created on: Jan 12, 2021
- *      Author: David    Original work by Jose (PTDreamer), 2017
+ *      Author: David    Original work by Jose Barros (PTDreamer), 2017
  */
 
 #ifndef GRAPHICS_GUI_OLED_H_
 #define GRAPHICS_GUI_OLED_H_
 
 #include "screen.h"
-
+extern screen_t *current_screen;
 void oled_addScreen(screen_t *screen, uint8_t index);
 void oled_draw(void);
 void oled_init(RE_Rotation_t (*Rotation)(RE_State_t*), RE_State_t *State);
