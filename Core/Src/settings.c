ce9192e20b2d7abf5a982506cd78b9560cef05f8
deividAlfa <48562135+deividAlfa@users.noreply.github.com>
2021-08-07 20:34:19 +0200

    This filter/pid settings work spot-on with every tip

--- Core/Src/settings.c
+++ Core/Src/settings.c
@@ -319,9 +319,9 @@ void resetCurrentProfile(void){
       systemSettings.Profile.tip[x].calADC_At_250   = T12_Cal250;
       systemSettings.Profile.tip[x].calADC_At_350   = T12_Cal350;     // These values are way lower, but better to be safe than sorry
       systemSettings.Profile.tip[x].calADC_At_450   = T12_Cal450;     // User needs to calibrate its station
-      systemSettings.Profile.tip[x].PID.Kp          = 3500;           // val = /1.000.000
-      systemSettings.Profile.tip[x].PID.Ki          = 7000;           // val = /1.000.000
-      systemSettings.Profile.tip[x].PID.Kd          = 1400;           // val = /1.000.000
+      systemSettings.Profile.tip[x].PID.Kp          = 7000;           // val = /1.000.000
+      systemSettings.Profile.tip[x].PID.Ki          = 6500;           // val = /1.000.000
+      systemSettings.Profile.tip[x].PID.Kd          = 2000;           // val = /1.000.000
       systemSettings.Profile.tip[x].PID.maxI        = 75;             // val = /100
       systemSettings.Profile.tip[x].PID.minI        = 0;              // val = /100
       systemSettings.Profile.tip[x].PID.tau         = 20;             // val = /100
@@ -392,9 +392,9 @@ void resetCurrentProfile(void){
     Error_Handler();  // We shouldn't get here!
   }
 
-  systemSettings.Profile.tipFilter.filter_normal            = 55;   // % of old data (more %, more filtering)
-  systemSettings.Profile.tipFilter.filter_partial           = 45;
-  systemSettings.Profile.tipFilter.filter_spikes            = 70;
+  systemSettings.Profile.tipFilter.filter_normal            = 65;   // % of old data (more %, more filtering)
+  systemSettings.Profile.tipFilter.filter_partial           = 50;
+  systemSettings.Profile.tipFilter.filter_spikes            = 60;
   systemSettings.Profile.tipFilter.filter_reset             = 0;
   systemSettings.Profile.tipFilter.partial_start            = 400;
   systemSettings.Profile.tipFilter.partial_end              = 600;
