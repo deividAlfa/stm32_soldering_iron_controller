f6b6d6274bbfe206b3500e92f19ffc772ae321b5
David <David@192.168.0.58>
2021-08-30 00:38:29 +0200

    Reduce runwaway power history to 3

--- Core/Src/iron.c
+++ Core/Src/iron.c
@@ -309,20 +309,28 @@ void configurePWMpin(uint8_t mode){
   HAL_GPIO_Init(PWM_GPIO_Port, &GPIO_InitStruct);
 }
 
+
 // Check iron runaway
+// Number of old power values stored to compute the average power.
+#define RUNAWAY_SZ  3
+
 void runAwayCheck(void){
   uint16_t TempStep,TempLimit;
   uint32_t CurrentTime = HAL_GetTick();
   uint16_t tipTemp = readTipTemperatureCompensated(old_reading, read_average);
-  static uint8_t pos,prev_power[4];
-  uint8_t power;
+  static uint8_t pos, prev_power[RUNAWAY_SZ];
+  uint8_t power = 0;
 
   if(systemSettings.setupMode==enable || (Iron.Error.safeMode && Iron.Error.active)){
     return;
   }
   prev_power[pos]=Iron.CurrentIronPower;                                                      // Circular buffer
-  if(++pos>3){ pos=0; }
-  power = ((uint16_t)prev_power[0]+prev_power[1]+prev_power[2]+prev_power[3])/4;              // Average of last 4 powers
+  if(++pos>RUNAWAY_SZ-1){ pos=0; }
+
+  for(uint8_t t=0; t<RUNAWAY_SZ; t++){
+    power += prev_power[t];
+  }
+  power /= RUNAWAY_SZ;
 
   // If by any means the PWM output is higher than max calculated, generate error
   if((Iron.Pwm_Out > (Iron.Pwm_Period+1)) || (Iron.Pwm_Out != __HAL_TIM_GET_COMPARE(Iron.Pwm_Timer,Iron.Pwm_Channel))){
@@ -336,7 +344,7 @@ void runAwayCheck(void){
     TempLimit = 930;
   }
 
-  if(power>1 && (Iron.RunawayStatus==runaway_ok)  && (Iron.DebugMode==disable) &&(tipTemp > Iron.CurrentSetTemperature)){
+  if(power && (Iron.RunawayStatus==runaway_ok)  && (Iron.DebugMode==disable) &&(tipTemp > Iron.CurrentSetTemperature)){
 
     if(tipTemp>TempLimit){ Iron.RunawayLevel=runaway_500; }
     else{
