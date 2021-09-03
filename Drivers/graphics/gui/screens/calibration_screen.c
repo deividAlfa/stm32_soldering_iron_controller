bf4bbd7e2658875526bc40f5156de4eff41672ba
David <David@DESKTOP-SHK0MNF>
2021-09-03 15:12:14 +0200

    Move button to fit longer word languages (Thanks AndyC52)

--- Drivers/graphics/gui/screens/calibration_screen.c
+++ Drivers/graphics/gui/screens/calibration_screen.c
@@ -445,7 +445,7 @@ static void Cal_Start_create(screen_t *scr) {
 
   newWidget(&w,widget_button,scr);
   Widget_Cal_Back=w;
-  w->posX = 84;
+  w->posX = 78;
   w->posY = 48;
   w->width = 42;
   ((button_widget_t*)w->content)->displayString=strings[lang]._STOP;
