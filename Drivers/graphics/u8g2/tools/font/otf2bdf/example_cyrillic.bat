85eb7e8817972579e9bdb7e2b8778e8cc45245a2
David <David@DESKTOP-SHK0MNF>
2021-09-04 17:51:06 +0200

    Add cyrillic example

---
+++ Drivers/graphics/u8g2/tools/font/otf2bdf/example_cyrillic.bat
@@ -0,0 +1,12 @@
+@echo off  
+set MIN=18
+set MAX=20
+set FONTS=	"arial.ttf"
+
+for %%F in (%FONTS%) do (
+     for /l %%S in (%MIN%, 1, %MAX%) do (
+	otf2bdf.exe -o "..\bdf\%%~F_%%S_cyrillic.bdf" -p %%S "..\ttf\%%~F" -l '1040_1103'
+	echo %%~F %%S
+    )
+)
+timeout 3 >nul
