50abc425abb64442102bc4548d0a93afef42733d
David <David@DESKTOP-SHK0MNF>
2021-09-05 02:10:48 +0200

    Increase tip temperature calculation accuracy to 0.1ºC

--- Drivers/generalIO/tempsensors.c
+++ Drivers/generalIO/tempsensors.c
@@ -7,8 +7,6 @@
 
 #include "tempsensors.h"
 #include "math.h"
-#define temp_minC  0                 // Minimum system temperature in degrees of Celsius
-#define temp_maxC  480                // Maximum calibration temperature in degrees of Celsius
 
 #ifdef ENABLE_INT_TEMP
 uint8_t use_int_temp;
@@ -66,7 +64,7 @@ int16_t readColdJunctionSensorTemp_x10(bool new, bool tempUnit){
       float pull_res=systemSettings.settings.Pull_res*100;
       float NTC_Beta;
       float adcValue=NTC.last_avg;
-      volatile float result;
+      float result;
 
       if(systemSettings.settings.NTC_detect){                                                           // NTC Autodetect enabled?
         NTC_res = systemSettings.settings.NTC_detect_low_res*100;                                       // Set lower by default
@@ -161,8 +159,8 @@ int16_t readTipTemperatureCompensated(bool new, bool mode){
     return 0;
   }
   if(new){
-    temp = adc2Human(TIP.last_avg,1,systemSettings.settings.tempUnit);
-    temp_Raw = adc2Human(TIP.last_raw,1,systemSettings.settings.tempUnit);
+    temp = adc2Human_x10(TIP.last_avg,1,systemSettings.settings.tempUnit)/10;
+    temp_Raw = adc2Human_x10(TIP.last_raw,1,systemSettings.settings.tempUnit)/10;
 
     // Limit output values
     if(temp>999){
@@ -199,59 +197,58 @@ tipData_t *getCurrentTip() {
 
 // Translate the human readable t into internal value
 int16_t human2adc(int16_t t) {
-  volatile int16_t temp = t;
-  volatile int16_t tH;
-  volatile int16_t ambTemp = readColdJunctionSensorTemp_x10(old_reading, mode_Celsius) / 10;
-
+  t = t*10;
   // If using Farenheit, convert to Celsius
   if(systemSettings.settings.tempUnit==mode_Farenheit){
-    t = TempConversion(t,mode_Celsius,0);
+    t = TempConversion(t,mode_Celsius,1);
   }
-  t-=ambTemp;
-  if (t < temp_minC){ return 0; }                                 // If requested temp below min, return 0
-  else if (t > temp_maxC){ t = temp_maxC; }                       // If requested over max, apply limit
+  int16_t temp = t;
+  t -= readColdJunctionSensorTemp_x10(old_reading, mode_Celsius);
+
+  if (t < 0){ return 0; }                                 // If requested temp below min, return 0
 
   // If t>350, map between ADC values Cal_350 - Cal_450
-  if (t >= 350){
-    temp = map(t, 350, 450, currentTipData->calADC_At_350, currentTipData->calADC_At_450);
+  if (t >= 3500){
+    temp = map(t, 3500, 4500, currentTipData->calADC_At_350, currentTipData->calADC_At_450);
   }
   // Else, map between ADC values Cal_250 - Cal_350
   else{
-     temp = map(t, 250, 350, currentTipData->calADC_At_250, currentTipData->calADC_At_350);
+     temp = map(t, 2500, 3500, currentTipData->calADC_At_250, currentTipData->calADC_At_350);
   }
-  tH = adc2Human(temp,0,mode_Celsius);
-  if (tH < (t-1)) {
-    while(tH < t){
-      tH = adc2Human(++temp,0,mode_Celsius);
+
+  int16_t tH = adc2Human_x10(temp,0,mode_Celsius);                // Find +0.5ºC to provide better reading stability
+  if (tH < (t+4)) {
+    while(tH < (t+4)){
+      tH = adc2Human_x10(++temp,0,mode_Celsius);
     }
   }
-  else if (tH > (t+1)) {
-    while(tH > t){
-      tH = adc2Human(--temp,0,mode_Celsius);
+  else if (tH > (t+6)) {
+    while(tH > (t+6)){
+      tH = adc2Human_x10(--temp,0,mode_Celsius);
     }
   }
   return temp;
 }
 
 // Translate temperature from internal units to the human readable value
-int16_t adc2Human(uint16_t adc_value,bool correction, bool tempUnit) {
-  int16_t tempH = 0;
-  int16_t ambTemp = readColdJunctionSensorTemp_x10(old_reading, mode_Celsius) / 10;
+int16_t adc2Human_x10(uint16_t adc_value,bool correction, bool tempUnit) {
+  int16_t temp;
+  int16_t ambTemp = readColdJunctionSensorTemp_x10(old_reading, mode_Celsius);
   if (adc_value >= currentTipData->calADC_At_350) {
-    tempH = map(adc_value, currentTipData->calADC_At_350, currentTipData->calADC_At_450, 350, 450);
+    temp = map(adc_value, currentTipData->calADC_At_350, currentTipData->calADC_At_450, 3500, 4500);
   }
   else{
-    tempH = map(adc_value, currentTipData->calADC_At_250, currentTipData->calADC_At_350, 250, 350);
+    temp = map(adc_value, currentTipData->calADC_At_250, currentTipData->calADC_At_350, 2500, 3500);
   }
-  if(correction){ tempH+= ambTemp; }
+  if(correction){ temp += ambTemp; }
   if(tempUnit==mode_Farenheit){
-    tempH=TempConversion(tempH,mode_Farenheit,0);
+    temp=TempConversion(temp,mode_Farenheit,1);
   }
-  return tempH;
+  return temp;
 }
 
-long map(long x, long in_min, long in_max, long out_min, long out_max) {
-  long ret;
+int32_t map(int32_t x, int32_t in_min, int32_t in_max, int32_t out_min, int32_t out_max) {
+  uint32_t ret;
   ret = (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
   if (ret < 0)
     ret = 0;
@@ -259,14 +256,12 @@ long map(long x, long in_min, long in_max, long out_min, long out_max) {
 }
 
 // Fixed point calculation
-// 2E20*1.8 = 1887437 , 2E20/1.8 = 582542
-// So (temp*1887437)>>20 == temp*1.8 (Real: 1,800000191)
-// (temp*582542)>>20 == temp/1.8 (Real: 1,800000687)
-// Max input = 1100°C / 3700°F, otherwise we will overflow the signed int32
-
+// So (temp*117965)>>10 == temp*1.8 (Real: 1,800003052)
+// (temp*36409)>>10 == temp/1.8 (Real: 1,799994507)
+// Max input: 110.000°F, 35.000°C (risk of uint32_t overflow)
 int16_t TempConversion(int16_t temperature, bool conversion, bool x10mode){
   if(conversion==mode_Farenheit){  // Input==Celsius, Output==Farenheit
-    temperature=(((int32_t)temperature*1887437)>>20);// F = (C*1.8)+32
+    temperature=(((int32_t)temperature*117965)>>16);// F = (C*1.8)+32
     if(x10mode){
       temperature += 320;
     }
@@ -281,7 +276,7 @@ int16_t TempConversion(int16_t temperature, bool conversion, bool x10mode){
     else{
       temperature -= 32;
     }
-    temperature=((int32_t)temperature*582542)>>20;// C = (F-32)/1.8
+    temperature=((int32_t)temperature*36409)>>16;// C = (F-32)/1.8
   }
   return temperature;
 }
--- Drivers/generalIO/tempsensors.h
+++ Drivers/generalIO/tempsensors.h
@@ -24,8 +24,8 @@ int16_t   readTipTemperatureCompensated(bool new, bool mode);
 void      setCurrentTip(uint8_t tip);
 tipData_t *getCurrentTip();
 
-long      map(long x, long in_min, long in_max, long out_min, long out_max);
-int16_t   adc2Human(uint16_t adc_value,bool correction, bool tempUnit);
+int32_t map(int32_t x, int32_t in_min, int32_t in_max, int32_t out_min, int32_t out_max);
+int16_t   adc2Human_x10(uint16_t adc_value,bool correction, bool tempUnit);
 int16_t   human2adc(int16_t t);
 int16_t   TempConversion(int16_t temperature, bool conversion, bool x10mode);
 
