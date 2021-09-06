560a68d802adf4dab1b19e21080ccb4b7edab83f
David <David@DESKTOP-SHK0MNF>
2021-09-06 11:10:19 +0200

    Add '-' symbol to IronTemp font

--- Drivers/graphics/u8g2/tools/font/bdfconv/make_font_iron_temp.bat
+++ Drivers/graphics/u8g2/tools/font/bdfconv/make_font_iron_temp.bat
@@ -1,1 +1,1 @@
-bdfconv_2_22.exe -v -b0 -f1 -m "48-57,67,70,176" "../bdf/ITC Avant Garde Gothic Medium_31.bdf" -o .\c\font_iron_temp.c  -n u8g2_font_iron_temp
+bdfconv_2_22.exe -v -b0 -f1 -m "45,48-57,67,70,176" "../bdf/ITC Avant Garde Gothic Medium_31.bdf" -o .\c\font_iron_temp.c  -n u8g2_font_iron_temp
