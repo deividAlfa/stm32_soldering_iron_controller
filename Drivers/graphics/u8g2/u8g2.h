5d5abf35de68ec7a42946a66a3e2fd7dbee287b7
David <David@DESKTOP-SHK0MNF>
2021-09-10 13:11:12 +0200

    Update fonts

--- Drivers/graphics/u8g2/u8g2.h
+++ Drivers/graphics/u8g2/u8g2.h
@@ -1517,7 +1517,7 @@ void u8g2_SetupBuffer_Utf8(u8g2_t *u8g2, const u8g2_cb_t *u8g2_cb);
 /* start font list */
 const uint8_t u8g2_font_small[1983] U8G2_FONT_SECTION("u8g2_font_small");
 const uint8_t u8g2_font_menu[2319] U8G2_FONT_SECTION("u8g2_font_menu");
-const uint8_t u8g2_font_no_iron_big[596] U8G2_FONT_SECTION("u8g2_font_no_iron_big");
+const uint8_t u8g2_font_no_iron_big[587] U8G2_FONT_SECTION("u8g2_font_no_iron_big");
 const uint8_t u8g2_font_iron_temp[620] U8G2_FONT_SECTION("u8g2_font_iron_temp");
 
 extern const uint8_t u8g2_font_u8glib_4_tf[] U8G2_FONT_SECTION("u8g2_font_u8glib_4_tf");
