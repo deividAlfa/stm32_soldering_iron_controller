655f51142814d9f79c22d9e3ad08cfc4d3c9cb51
David <David@DESKTOP-SHK0MNF>
2021-09-29 06:22:21 +0200

    Update script

--- Clean_Profile.bat
+++ Clean_Profile.bat
@@ -1,15 +1,9 @@
 @echo off
-del .\Core\Inc\stm*.* /Q >nul
-del .\Core\Inc\board.h /Q >nul
-del .\Core\Src\stm*.* /Q >nul
-del .\Core\Src\system_stm*.* /Q >nul
-del .\Core\Startup\*.s /Q >nul
-del .\.cproject /Q >nul
-del .\.project /Q >nul
-del .\STM32SolderingStation.ioc /Q >nul
-rmdir .\Drivers\STM32F0xx_HAL_Driver /S /Q >nul
-rmdir .\Drivers\STM32F1xx_HAL_Driver /S /Q >nul
-rmdir .\Debug /S /Q >nul
-rmdir .\Release /S /Q >nul
-echo Done cleaning profile files!
-ping localhost -n 2 >nul
+del Core\Inc\board.h Core\Inc\*stm32*.* Core\Src\*stm32*.* Core\Startup\*.s .cproject .project *.ioc *.bin /Q 2>nul
+rd Debug /Q /S 2>nul
+rd Release /Q /S /Q 2>nul
+
+if /I not "%1"=="-s" (
+    echo Done cleaning profile files!
+    timeout 3 >nul
+)
