9813b429d99ebc69aa1043225f70e1f096ec7089
David <David@DESKTOP-SHK0MNF>
2021-10-01 11:14:18 +0200

    Remove noIron_Big font

--- Drivers/graphics/u8g2/u8g2.h
+++ Drivers/graphics/u8g2/u8g2.h
@@ -1517,7 +1517,6 @@ void u8g2_SetupBuffer_Utf8(u8g2_t *u8g2, const u8g2_cb_t *u8g2_cb);
 /* start font list */
 const uint8_t u8g2_font_small[1988] U8G2_FONT_SECTION("u8g2_font_small");
 const uint8_t u8g2_font_menu[2330] U8G2_FONT_SECTION("u8g2_font_menu");
-const uint8_t u8g2_font_no_iron_big[633] U8G2_FONT_SECTION("u8g2_font_no_iron_big");
 const uint8_t u8g2_font_iron_temp[624] U8G2_FONT_SECTION("u8g2_font_iron_temp");
 
 extern const uint8_t u8g2_font_u8glib_4_tf[] U8G2_FONT_SECTION("u8g2_font_u8glib_4_tf");
