a6b3c384994e2b9c51faadf9b903fe3cdb347750
deividAlfa <48562135+deividAlfa@users.noreply.github.com>
2021-10-09 23:18:22 +0200

    Fix typo in comments

--- Core/Inc/settings.h
+++ Core/Inc/settings.h
@@ -12,12 +12,12 @@
 #include "pid.h"
 #include "board.h"
 
-#define SWSTRING          "SW: 21-10-09"                            // Software version reported in settings creen
+#define SWSTRING          "SW: 21-10-09"                            // Software version reported in settings screen
 #define SETTINGS_VERSION  14                                        // Change this if you change the settings/profile struct to prevent getting out of sync
 #define LANGUAGE_COUNT    5                                         // Number of languages
 #define ProfileSize       3                                         // Number of profiles
 #define TipSize           20                                        // Number of tips for each profile
-#define TipCharSize       5                                         // String size for each tip name (Including null terminator)
+#define TipCharSize       5                                         // String size for each tip name (Including null termination)
 #define _BLANK_TIP        "    "                                    // Empty tip name, 4 spaces. Defined here for quick updating if TipCharSize is modified.
 
 #ifndef PROFILE_VALUES
