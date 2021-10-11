39b82e49f952d2cfe3e53d6063bcaac886545ff5
deividAlfa <48562135+deividAlfa@users.noreply.github.com>
2021-10-12 00:10:57 +0200

    Add working tip selection from tip list screen using long-lick

--- Drivers/graphics/gui/screens/tip_list_screen.c
+++ Drivers/graphics/gui/screens/tip_list_screen.c
@@ -10,7 +10,7 @@
 screen_t Screen_tip_list;
 
 static comboBox_item_t *comboitem_tip_list_addNewTip;
-
+static comboBox_item_t *comboitem_tip_list_back;
 
 static int addNewTip(widget_t *w, RE_Rotation_t input){
   newTip=1;
@@ -39,7 +39,22 @@ static void tip_list_init(screen_t *scr) {
   comboitem_tip_list_addNewTip->enabled = (systemSettings.Profile.currentNumberOfTips < TipSize);
 }
 
-
+int tip_list_ProcessInput(screen_t * scr, RE_Rotation_t input, RE_State_t *state){
+  comboBox_item_t *item = ((comboBox_widget_t*)scr->current_widget->content)->currentItem;
+  if(input==LongClick){
+    if( item!=comboitem_tip_list_addNewTip && item!=comboitem_tip_list_back){                         // If long click over a tip
+      for(uint8_t i=0; i<systemSettings.Profile.currentNumberOfTips; i++){                            // Find selected tip
+        if(strcmp(item->text, systemSettings.Profile.tip[i].name) == 0){                              // If matching name
+          setCurrentTip(i);                                                                           // Set tip
+          systemSettings.Profile.currentTip = i;
+          break;
+        }
+      }
+      return screen_main;                                                                             // Exit to main screen
+    }
+  }
+  return (autoReturn_ProcessInput(scr,input,state));
+}
 static void tip_list_create(screen_t *scr){
   widget_t* w;
   //  [ IRON TIPS COMBO ]
@@ -50,12 +65,12 @@ static void tip_list_create(screen_t *scr){
     newComboAction(w, " ", &editTip, NULL);              // Names filled when entering the menu
   }
   newComboAction(w, strings[lang]._ADD_NEW, &addNewTip, &comboitem_tip_list_addNewTip);
-  newComboScreen(w, strings[lang]._BACK, screen_settings, NULL);
+  newComboScreen(w, strings[lang]._BACK, screen_settings, &comboitem_tip_list_back);
 }
 
 
 void tip_list_screen_setup(screen_t *scr){
   scr->init = &tip_list_init;
-  scr->processInput=&autoReturn_ProcessInput;
+  scr->processInput=&tip_list_ProcessInput;
   scr->create=&tip_list_create;
 }
