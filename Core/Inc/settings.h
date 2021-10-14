9bad0cd1d958499ebe73ac7621fdbd01f74f5f44
deividAlfa <48562135+deividAlfa@users.noreply.github.com>
2021-10-14 23:55:48 +0200

    Update version

--- Core/Inc/settings.h
+++ Core/Inc/settings.h
@@ -12,7 +12,7 @@
 #include "pid.h"
 #include "board.h"
 
-#define SWSTRING          "SW: 21-10-12a"                           // Software version reported in settings screen
+#define SWSTRING          "SW: 21-10-14"                            // Software version reported in settings screen
 #define SETTINGS_VERSION  14                                        // Change this if you change the settings/profile struct to prevent getting out of sync
 #define LANGUAGE_COUNT    5                                         // Number of languages
 #define ProfileSize       3                                         // Number of profiles
