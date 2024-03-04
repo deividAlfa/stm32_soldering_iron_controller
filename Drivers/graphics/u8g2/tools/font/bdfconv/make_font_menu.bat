5d678a8e1c328d59ac6de7b2ed9a4f98ab7ebfbd
David <David@DESKTOP-GD7CRKH>
2024-03-04 01:03:08 +0100

    Update Czech translation

--- Drivers/graphics/u8g2/tools/font/bdfconv/make_font_menu.bat
+++ Drivers/graphics/u8g2/tools/font/bdfconv/make_font_menu.bat
@@ -1,3 +1,3 @@
 echo.
 md out>nul
-bdfconv.exe -v -b0 -f1 -m "32-126,176,196,197,199,214,220,228,229,231,246,252,286,287,304,305,350,351,937,1040-1103" "../bdf/font_menu.bdf" -o ./out/font_menu.c  -n u8g2_font_menu
+bdfconv.exe -v -b0 -f1 -m "32-126,160-255,268-271,282-283,286,287,304,305,327,328,344,345,350-353,356,357,366,367,381,382,937,1040-1103" "../bdf/font_menu.bdf" -o ./out/font_menu.c  -n u8g2_font_menu
