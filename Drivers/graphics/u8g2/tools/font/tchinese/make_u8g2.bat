5453ff13f2194a6b09c50daf69280225664b49bc
sst311212 <sst311212@gmail.com>
2024-03-05 01:05:02 +0800

    Fix: otf2bdf access TEMP instead TMPDIR in Windows
    0x4D18: TMPDIR -> TEMP

--- Drivers/graphics/u8g2/tools/font/tchinese/make_u8g2.bat
+++ Drivers/graphics/u8g2/tools/font/tchinese/make_u8g2.bat
@@ -1,13 +1,9 @@
 @echo off
 pushd "%~dp0"
-python make_bdfmap.py
 
-pushd "%SystemDrive%"
-mkdir tmp
-"%~dp0\..\otf2bdf\otf2bdf.exe" -m "%~dp0\font_menu_cht.map" -p 9 -o "%~dp0\font_menu_cht.bdf" "%~dp0\fireflysung.ttf"
-"%~dp0\..\otf2bdf\otf2bdf.exe" -m "%~dp0\font_small_cht.map" -p 8 -o "%~dp0\font_small_cht.bdf" "%~dp0\fireflysung.ttf"
-rmdir tmp
-popd
+python make_bdfmap.py
+..\otf2bdf\otf2bdf.exe -m font_menu_cht.map -p 9 -o font_menu_cht.bdf fireflysung.ttf
+..\otf2bdf\otf2bdf.exe -m font_small_cht.map -p 8 -o font_small_cht.bdf fireflysung.ttf
 
 python make_bdfmux.py
 ..\bdfconv\bdfconv.exe -v -b 0 -f 1 -m "32-126,160-255,268-271,282-283,286,287,304,305,327,328,344,345,350-353,356,357,366,367,381,382,937,1040-1103,5000-65535" -o font_menu.c -n u8g2_font_menu font_menu_all.bdf
